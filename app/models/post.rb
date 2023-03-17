3042c0c2e8082fbf5bab9e24cbe4f3fc4372b2ce
+ <+>
2023-03-17 23:49:14 +0300

    Assignment Done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validate :clickbait_title
+
+  validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
+
+  
+
+  def clickbait_title
+    
+        if title.blank? || !title.include?("Won't Believe") && !title.include?("Secret") && !title.include?("Top") && !title.include?("Guess")
+          errors.add(:title, "must be clickbait-y!")
+        end
+      end
 end
